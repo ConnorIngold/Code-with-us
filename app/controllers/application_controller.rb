8eefa2a433cae16eb9f486ad1fb355b26f4798e7
Samuel Gasper <1516828@my.brunel.ac.uk>
2018-03-07 16:15:38 +0000

    Saving progress before testing something

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,15 @@ class ApplicationController < ActionController::Base
   end
 
 
+  def after_sign_in_path_for(resource)
+   root_path
+  end
+
+  def after_sign_up_path_for(resource)
+   root_path
+  end
+
+
   before_action :authenticate_user!
   include Pundit
 
