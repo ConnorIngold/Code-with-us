1370acc11685dc9d08be1de02b0a3e41f42d237b
Sumai Khayat <sumaikhayat@gmail.com>
2018-03-08 19:59:02 +0000

    added more design and js logic

--- app/javascript/packs/target.js
+++ app/javascript/packs/target.js
@@ -4,13 +4,17 @@ console.log(ProgressBar);
 // Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
 var svgPath = document.getElementById('progress');
 var bar = new ProgressBar.Path(svgPath, {
-  strokeWidth: 1000,
   easing: 'easeInOut',
   duration: 3000,
-  color: '#7FD89B',
-  trailColor: '#eee',
-  trailWidth: 1
+  // step: function() {
+  //   strokeWidth: 10,
+  //   trailWidth: 2
+  // }
 });
 
 
 bar.animate(1.0);  // Number from 0.0 to 1.0
+// strokeWidth: 10,
+//   trailWidth: 2,
+//   color: '#7FD89B',
+//   trailColor: '#eee'
