8828ef1819b80ef7fcef15eabe80f7629aecdcd6
Sumai Khayat <sumaikhayat@gmail.com>
2018-03-08 16:52:48 +0000

    added more tab logic to project page

--- app/javascript/packs/target.js
+++ app/javascript/packs/target.js
@@ -9,31 +9,7 @@ var bar = new ProgressBar.Path(svgPath, {
   duration: 3000,
   color: '#7FD89B',
   trailColor: '#eee',
-  trailWidth: 1,
-<<<<<<< HEAD
-  svgStyle: {width: '100%', height: '100%'},
-  text: {
-    style: {
-      // Text color.
-      // Default: same as stroke color (options.color)
-      color: '#999',
-      position: 'absolute',
-      right: '0',
-      top: '30px',
-      padding: 10,
-      margin: 5,
-      transform: null
-    },
-    autoStyleContainer: false
-  },
-  from: {color: '#FFEA82'},
-  to: {color: '#ED6A5A'},
-  step: (state, bar) => {
-    bar.setText(Math.round(bar.value() * 100) + ' %');
-  }
-=======
-  svgStyle: {width: '100%', height: '100%'}
->>>>>>> aad0bdf429439c4c3d871247bb5707f51a4caa75
+  trailWidth: 1
 });
 
 
