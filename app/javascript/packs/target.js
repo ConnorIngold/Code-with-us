4cfa1df53e5705a431d6e039d2757a2ccd3427bd
Samuel Gasper <1516828@my.brunel.ac.uk>
2018-03-09 12:17:16 +0000

    Done the banner and styled the search bar some more

--- app/javascript/packs/target.js
+++ app/javascript/packs/target.js
@@ -4,37 +4,15 @@ console.log(ProgressBar);
 // Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
 var svgPath = document.getElementById('progress');
 var bar = new ProgressBar.Path(svgPath, {
-  strokeWidth: 1000,
   easing: 'easeInOut',
   duration: 3000,
-  color: '#7FD89B',
-  trailColor: '#eee',
-  trailWidth: 1,
-<<<<<<< HEAD
-  svgStyle: {width: '100%', height: '100%'},
-  text: {
-    style: {
-      // Text color.
-      // Default: same as stroke color (options.color)
-      color: '#999',
-      position: 'absolute',
-      right: '0',
-      top: '30px',
-      padding: 10,
-      margin: 5,
-      transform: null
-    },
-    autoStyleContainer: false
-  },
-  from: {color: '#FFEA82'},
-  to: {color: '#ED6A5A'},
-  step: (state, bar) => {
-    bar.setText(Math.round(bar.value() * 100) + ' %');
-  }
-=======
-  svgStyle: {width: '100%', height: '100%'}
->>>>>>> aad0bdf429439c4c3d871247bb5707f51a4caa75
+  // step: function() {
+  //   strokeWidth: 10,
+  //   trailWidth: 2
+  // }
 });
-
-
 bar.animate(1.0);  // Number from 0.0 to 1.0
+// strokeWidth: 10,
+//   trailWidth: 2,
+//   color: '#7FD89B',
+//   trailColor: '#eee'
