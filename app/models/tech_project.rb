ec519aa1a0b1a9e91a6779d0322a7897b0f7d6e3
Alexander Sips-Avello <alex.s.a1994@gmail.com>
2018-03-12 16:18:20 +0000

    changed something quick

--- app/models/tech_project.rb
+++ app/models/tech_project.rb
@@ -1,19 +1,4 @@
 class TechProject < ApplicationRecord
   belongs_to :project
   belongs_to :technology
-
-    def dif_colour
-      if self.difficulty == 1
-        "dif_1"
-      elsif self.difficulty == 2
-        "dif_2"
-      elsif self.difficulty == 3
-        "dif_3"
-      elsif self.difficulty == 4
-        "dif_4"
-      elsif self.difficulty ==  5
-        "dif_5"
-      end
-    end
-  end
 end
