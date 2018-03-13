4bb66c4850c2ffdfc6d9b84ba602147b5863546a
Alexander Sips-Avello <alex.s.a1994@gmail.com>
2018-03-13 14:49:01 +0000

    hopefully it is fixed now

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -11,6 +11,7 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
     @task = Task.new
+    @message = Message.new
     authorize @project
   end
 
