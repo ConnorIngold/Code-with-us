ee432ea619175116de5914f185ac0e84193ca329
Alexander Sips-Avello <alex.s.a1994@gmail.com>
2018-03-14 18:05:37 +0000

    random shit

---
+++ app/javascript/packs/messagePolling.js
@@ -0,0 +1,11 @@
+// fire a get request at messages/get_new_messages
+// cal every 5 seconds
+// if news messages then should return them
+// Then append messages to the DOM
+// console.log('lolz')
+// fetch(`${window.location.href}/messages/get_new_messages`)
+//   .then(response => response.json())
+//   .then((data) => {
+//     // insert into DOM here!
+//     console.log(data);
+//   });
